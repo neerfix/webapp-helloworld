6fff47982bd9249c783682837e84687ac022cadf
arty3p <gsanchez.dev@gmail.com>
2022-02-14 15:46:19 +0100

    feat (init): config colors in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 module.exports = {
 	content: [
-		"./src/**/*.{js,jsx}"
+		'./src/**/*.{js,jsx}'
 	],
 	theme: {
-		extend: {},
+		colors: {
+			'white': '#FCFAF7',
+			'black': '#291D11',
+			'gray': '#D1CECB',
+			'green': '#567353'
+		},
+		extend: {
+			colors: {
+				'light-blue': '#AABFBE',
+				'light-green': '#748971',
+				'brown': '#867065',
+				'light-brown': '#B78E64',
+				'dark-brown': '#7B6253',
+				'beige': '#F4E9DD',
+				'salmon': '#FF9B70'
+			}
+		},
 	},
 	plugins: [],
 }
