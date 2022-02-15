995d1c7b8748872e0cedf7dbd6100d377bff161f
arty3p <gsanchez.dev@gmail.com>
2022-02-15 12:14:05 +0100

    feat (footer): place header and footer on all views / structure in progress

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,25 @@ import React from 'react';
 class Footer extends React.Component {
     render() {
         return (
-            <div>
-                HelloWorld
+            <div className={"footer border-t-2 border-dark-brown bg-beige px-16 py-4"}>
+                <div className={"flex items-center justify-between"}>
+                    <div></div>
+                    <div>
+                        <div className={"flex items-center justify-start"}>
+                            <span></span>
+                            <span>Carte</span>
+                        </div>
+                        <div className={"flex items-center justify-start"}>
+                            <span></span>
+                            <span>Carnet</span>
+                        </div>
+                        <div className={"flex items-center justify-start"}>
+                            <span></span>
+                            <span>Passport</span>
+                        </div>
+                    </div>
+                </div>
+                <div></div>
             </div>
         );
     }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+// Components
 import DestinationCard from '@/components/DestinationCard'
 
 // Images
@@ -7,27 +6,23 @@ import HeroBannerImg from '@/assets/images/homepage/hero-banner.jpg';
 
 const HomePage = () => {
 	return (
-		<div>
-			<Header />
-			<main id="homepage">
-				<div className="bloc">
-					<h1 id="main-title">HelloWorld</h1>
-					<img height="1000" id="main-img" src={HeroBannerImg} alt="" />
-				</div>
-				<div className="bloc">
-					Découvrir le monde
-				</div>
-				<div>
-					<h3>Destination populaires</h3>
-					<DestinationCard 
-						title="Rome" country="Italie" link="/roma" img="https://picsum.photos/id/123/145/145" />
-					<DestinationCard 
-						title="New York" country="Etats-Unis" link="/new-york" img="https://picsum.photos/id/115/145/145" />
-					<DestinationCard 
-						title="Tahunanui" country="Nouvelle Zélande" link="/new-zealand" img="https://picsum.photos/id/175/145/145" />
-				</div>
-			</main>
-			<Footer />
+		<div id="homepage">
+			<div className="bloc">
+				<h1 id="main-title">HelloWorld</h1>
+				<img height="1000" id="main-img" src={HeroBannerImg} alt="" />
+			</div>
+			<div className="bloc">
+				Découvrir le monde
+			</div>
+			<div>
+				<h3>Destination populaires</h3>
+				<DestinationCard
+					title="Rome" country="Italie" link="/roma" img="https://picsum.photos/id/123/145/145" />
+				<DestinationCard
+					title="New York" country="Etats-Unis" link="/new-york" img="https://picsum.photos/id/115/145/145" />
+				<DestinationCard
+					title="Tahunanui" country="Nouvelle Zélande" link="/new-zealand" img="https://picsum.photos/id/175/145/145" />
+			</div>
 		</div>
 	)
 }
