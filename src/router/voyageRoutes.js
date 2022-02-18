fc2da208ea9d2e3455d66ac7505f657eabee82a4
Aimee <aimeeritleng@outlook.com>
2022-02-18 15:56:00 +0100

    feat(voyage): edition page

--- src/router/voyageRoutes.js
+++ src/router/voyageRoutes.js
@@ -1,9 +1,11 @@
 
 /** Pages **/
 import VoyagePage from "@/pages/Voyage/Voyage";
+import VoyageEditPage from "@/pages/Voyage/VoyageEdit";
 
 const routes = [
-	{ path: '/voyage/:id_voyage', element: <VoyagePage /> }
+	{ path: '/voyage/:id_voyage', element: <VoyagePage /> },
+	{ path: '/voyage/edit/:id_voyage', element: <VoyageEditPage /> }
 ];
 
 export default routes;
