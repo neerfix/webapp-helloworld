7f6b5d2155e90d49f248b9e66e3bc4ceae31f7e0
emelinepal <emeline43@outlook.fr>
2022-02-18 14:22:20 +0100

    feat : map searchbar

--- src/pages/public/InteractiveMap.jsx
+++ src/pages/public/InteractiveMap.jsx
@@ -1,12 +1,14 @@
 // Icones
 //import { VscChevronDown } from 'react-icons/vsc';
 import Map from '@/components/Map'
+import SearchBar from '@/components/SearchBar'
 
 const InteractiveMap = () => {
 	return (
 		<div className='bg-beige'>
 			<main id="interactive-map" className="w-full">
 				<Map />
+				<SearchBar />
 			</main>
 		</div>
 	)
