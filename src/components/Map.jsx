cb2f0257b4b20926b698a5e4d0cfcf5ef4f2f1ca
Aimee <aimeeritleng@outlook.com>
2022-03-07 11:10:05 +0100

    fix(map): add filter ctrl btn & fix style

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -15,10 +15,101 @@ const Map = () => {
 
         map.addControl(new mapboxgl.NavigationControl({
             showCompass: false
-        }), 'top-right');
+        }), 'bottom-right');
 
         var geocoder = new MapboxGeocoder({ accessToken: mapboxgl.accessToken });
         map.addControl(geocoder, 'top-left');
+
+        class MapboxGLButtonControl {
+            constructor({
+              className = "",
+              title = "",
+              eventHandler = null
+            }) {
+              this._className = className;
+              this._title = title;
+              this._eventHandler = eventHandler;
+            }
+
+            onAdd(map) {
+                this._btn = document.createElement("button");
+                this._btn.className = "mapboxgl-ctrl-icon" + " " + this._className;
+                this._btn.type = "button";
+                this._btn.title = this._title;
+                this._btn.onclick = this._eventHandler;
+            
+                this._container = document.createElement("div");
+                this._container.className = "mapboxgl-ctrl-group mapboxgl-ctrl";
+                this._container.appendChild(this._btn);
+            
+                return this._container;
+            }
+            
+            onRemove() {
+            this._container.parentNode.removeChild(this._container);
+            this._map = undefined;
+            }
+        }
+
+        /* Instantiate new controls with custom event handlers */
+        const filterMapBtn = new MapboxGLButtonControl({
+            className: "filter-map-control",
+            title: "Filtrer",
+            eventHandler: filterMapEvent
+        });
+
+        function filterMapEvent(event) {
+            console.log("Filtrage de la map");
+        }
+
+        map.addControl(filterMapBtn, 'top-right');
+
+        class PitchToggle {
+            constructor({ bearing = -20, pitch = 70, minpitchzoom = null }) {
+                this._bearing = bearing;
+                this._pitch = pitch;
+                this._minpitchzoom = minpitchzoom;
+            }
+
+            onAdd(map) {
+                this._map = map;
+                let _this = this;
+
+                this._btn = document.createElement("button");
+                this._btn.className = "mapboxgl-ctrl-icon mapboxgl-ctrl-pitchtoggle-3d";
+                this._btn.type = "button";
+                this._btn["aria-label"] = "Activer la vue 3D";
+                this._btn.onclick = function () {
+                    if (map.getPitch() === 0) {
+                        let options = { pitch: _this._pitch, bearing: _this._bearing };
+                        if (_this._minpitchzoom && map.getZoom() > _this._minpitchzoom) {
+                            options.zoom = _this._minpitchzoom;
+                        }
+                        map.easeTo(options);
+                        _this._btn.className =
+                            "mapboxgl-ctrl-icon mapboxgl-ctrl-pitchtoggle-2d";
+                    } else {
+                        map.easeTo({ pitch: 0, bearing: 0 });
+                        _this._btn.className =
+                            "mapboxgl-ctrl-icon mapboxgl-ctrl-pitchtoggle-3d";
+                    }
+                };
+
+                this._container = document.createElement("div");
+                this._container.className = "mapboxgl-ctrl-group mapboxgl-ctrl";
+                this._container.appendChild(this._btn);
+
+                return this._container;
+            }
+
+            onRemove() {
+                this._container.parentNode.removeChild(this._container);
+                this._map = undefined;
+            }
+        }
+        
+        map.addControl(new PitchToggle({ minpitchzoom: 4 }), "bottom-left");
+  
 
     }, []);
 
