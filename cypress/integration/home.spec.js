2bf1a4e6035b64aa4805e0a9483cfaee6cdb4a2f
arty3p <gsanchez.dev@gmail.com>
2022-03-08 11:18:41 +0100

    feat (cypress) : github action store screenshots and videos (5)

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -3,6 +3,10 @@ describe('Check Home Page content', function() {
 		cy.visit('/')
 	})
 	
+	it('Travels Map', () => {
+		cy.get('#bloc-map .title-centered h2').contains("Carte des voyages")
+	})
+	
 	it('Popular Travels', () => {
 		cy.get('#bloc-popular .title-container h2').contains("Destinations populaires")
 		cy.get('#bloc-map .title-centered h2').contains("Carte des voyages")
